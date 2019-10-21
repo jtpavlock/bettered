fe53fd065fdfdb8f54d9dd8682767039c0355441
Jacob <jtpavlock@gmail.com>
2019-10-20 21:01:59 -0700

    ability via command line argument to transcode to v0, 320 or both

--- betterRED.py
+++ betterRED.py
@@ -9,16 +9,21 @@ from tinytag import TinyTag
 
 def main():
     args = parse_args()
-    transcode(args.flac_path)
 
+    for mp3_type in args.mp3_type:
+        transcode(args.flac_path, mp3_type)
+
+# TODO: add option for 320 vs v0
 def parse_args():
     parser = argparse.ArgumentParser()
 
+    parser.add_argument('mp3_type', choices=['v0','320'], nargs='+',
+                        help='MP3 type to transcode to (default: MP3 320)')
     parser.add_argument('flac_path', help='Path to flac folder')
 
     return parser.parse_args()
 
-def transcode(flac_path):
+def transcode(flac_path, mp3_type):
 
     # copy everything to new destination
     output_folder_parent = '~/uploads/music'
@@ -28,7 +33,8 @@ def transcode(flac_path):
             if file.endswith('.flac'):
                 tag = TinyTag.get(os.path.join(root, file))
                 output_folder = (tag.albumartist + ' - ' + tag.album
-                                 + ' (' + tag.year + ') ' + ' [MP3 320]')
+                                 + ' (' + tag.year + ') '
+                                 + ' [MP3 ' + mp3_type + ']')
                 break
 
     output_path = os.path.expanduser(os.path.join(output_folder_parent, output_folder))
@@ -37,19 +43,21 @@ def transcode(flac_path):
     if not os.path.exists(output_path):
         copy_tree(flac_path, output_path)
 
+    transcode_opts = ['-aq', '0'] if mp3_type is 'V0' else ['-ab', '320k']
+
     # transcode all flac files
+    os.chdir(output_path)
+    print(os.getcwd())
     for root, dirs, files in os.walk(output_path):
         for file in files:
             if file.endswith('.flac'):
                 # transcode
-                flac_file = os.path.join(root, file)
-                mp3_file = os.path.join(root, file.replace('.flac', '.mp3'))
+                f = os.path.join(root, file)
+                mp3_file = os.path.join(root, f.replace('.flac', '.mp3'))
                 subprocess.run(['ffmpeg',
-                                '-i', flac_file,
-                                '-ab', '320k',
-                                mp3_file])
-                os.remove(flac_file)
-
+                                '-i', f] + transcode_opts
+                               + [f.replace('.flac', '.mp3')])
+                os.remove(f)
 
 if __name__ == "__main__":
     main()
