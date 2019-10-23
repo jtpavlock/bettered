bbb6166dc6d2daca8a0ceb5d448c95f913133eba
Jacob <jtpavlock@gmail.com>
2019-10-22 19:16:46 -0700

    add config file

--- better_red.py
+++ better_red.py
@@ -10,8 +10,7 @@ Example:
     $ betterRED v0 320 "Abbey Road [FLAC]/"
 
 Todo:
-    * Finish docstrings (setup yasnippet?)
-    * Clean up pylint / flake8 errors
+    * Test uploading
 """
 
 import os
@@ -19,6 +18,7 @@ import argparse
 import subprocess
 import shlex
 from distutils.dir_util import copy_tree
+from configparser import ConfigParser
 
 from tinytag import TinyTag
 
@@ -26,82 +26,112 @@ from tinytag import TinyTag
 def main():
     """Run that shit."""
     args = parse_args()
+    config = ConfigParser()
+    config.read(os.path.expanduser('~/.config/betterRED/config.ini'))
 
-    for mp3_type in args.mp3_type:
-        output_dir_basename = create_output_dir_basename(args.flac_path,
-                                                         mp3_type)
-        mp3_path = transcode(args.flac_path, mp3_type, output_dir_basename)
-        make_torrent(mp3_path, output_dir_basename)
+    for mp3_bitrate in args.mp3_bitrate:
+        mp3_dir = create_pathname(args.flac_dir, mp3_bitrate,
+                                  config.get('transcode', 'output_dir'))
+
+        transcode(args.flac_dir, mp3_bitrate, mp3_dir)
+
+        torrent_dir = os.path.join(config.get('torrent', 'torrent_file_dir'),
+                                   os.path.basename(mp3_dir))
+        make_torrent(mp3_dir, torrent_dir,
+                     config.get('redacted', 'announce_id'))
 
 
 def parse_args():
     """Parses and returns commandline arguments."""
     parser = argparse.ArgumentParser(description=__doc__)
 
-    parser.add_argument('mp3_type', choices=['v0', '320'], nargs='+',
+    parser.add_argument('mp3_bitrate', choices=['V0', '320'], nargs='+',
                         help='MP3 type to transcode to (default: MP3 320)')
-    parser.add_argument('flac_path', help='Path to flac folder')
+    parser.add_argument('flac_dir', help='Path to flac folder')
 
     return parser.parse_args()
 
 
-def create_output_dir_basename(flac_path, mp3_type):
-    """Creates basename of output directory.
+def create_pathname(flac_dir: str, mp3_bitrate: str, parent_dir: str) -> str:
+    """Creates a pathname with a album-formatted basename and given parent dir.
 
-    Uses basic metadata from a single flac file to name the folder as such:
-    'AlbumArtist - Album (Year) [Codec]'
+    Uses embedded metadata to name the basename directory as such:
+    'AlbumArtist - Album (Year) [MP3 mp3_bitrate]'
 
+    Args:
+        flac_dir: Directory containing a flac to grab metadata tags from.
+        mp3_bitrate: Mp3 bitrate (used in directory name formatting)
+        parent_dir: Parent directory to be combined with the created basename.
+
+    Returns:
+        The full pathname.
     """
-
-    for root, __, files in os.walk(flac_path):
+    for root, __, files in os.walk(flac_dir):
         for file in files:
             if file.endswith('.flac'):
-                tag = TinyTag.get(os.path.join(root, file))
-                output_title = (tag.albumartist + ' - ' + tag.album
-                                + ' (' + tag.year + ') '
-                                + ' [MP3 ' + mp3_type + ']')
-                return output_title
+                tags = TinyTag.get(os.path.join(root, file))
 
-    raise Exception(f'No flac files were found in {flac_path}')
+                basename = (f'{tags.albumartist} - {tags.album} ({tags.year})'
+                            f' [MP3 {mp3_bitrate}]')
+                return os.path.join(parent_dir, basename)
+
+    raise Exception(f'No flac files were found in {flac_dir}')
 
 
-def transcode(flac_path, mp3_type, output_title):
-    """Transcode flac to mp3"""
+def transcode(flac_dir: str, mp3_bitrate: str, mp3_dir: str):
+    """Transcode flac dir to mp3.
 
-    output_dir = '/home/jacob/uploads/music'
-    output_path = os.path.join(output_dir, output_title)
+    Transcodes a given directory of flac files to specified mp3 bitrate and
+    renames the directory according to a given title. Also copies all non-flac
+    files (e.g. cue, log, images, etc.) to the resulting directory.
+    Intended to be used with an album of flacs.
 
-    # temporary check if output path exists (usually just break altogether)
-    if not os.path.exists(output_path):
-        copy_tree(flac_path, output_path)  # copy everything over
+    Args:
+        flac_dir: Path to directory containing flac files to be converted.
+        mp3_bitrate: Mp3 bitrate to transcode to.
+            Supported: V0, 320
+        mp3_dir: Where to store the new mp3 files.
 
-    transcode_opts = ['-aq', '0'] if mp3_type == 'V0' else ['-ab', '320k']
+    Raises:
+        Exception: If mp3_dir already exists.
+    """
+
+    if os.path.exists(mp3_dir):
+        raise Exception(f'Output directory {mp3_dir} already exists')
+
+    copy_tree(flac_dir, mp3_dir)  # copy everything over
+
+    transcode_opts = '-aq 0' if mp3_bitrate == 'V0' else '-ab 320k'
 
     # transcode all flac files
-    for root, __, files in os.walk(output_path):
+    for root, __, files in os.walk(mp3_dir):
         for file in files:
             if file.endswith('.flac'):
                 flac_file = os.path.join(root, file)
+                mp3_file = flac_file.replace('.flac', '.mp3')
 
                 # use ffmpeg to transcode
-                subprocess.run(['ffmpeg', '-i', flac_file] + transcode_opts
-                               + [flac_file.replace('.flac', '.mp3')],
-                               check=True)
+                transcode_cmd = (f'ffmpeg -i "{flac_file}" {transcode_opts}'
+                                 f' "{mp3_file}"')
+
+                subprocess.run(shlex.split(transcode_cmd), check=True)
 
                 os.remove(flac_file)
 
-    return output_path
 
+def make_torrent(input_dir: str, torrent_dir: str, announce_id: str):
+    """Makes torrent file for a given directory for upload to redacted.
 
-def make_torrent(mp3_path, output_title):
-    """Make torrent file for transcoded mp3s."""
+    Args:
+        input_dir: Directory of music files to create the torrent from.
+        torrent_dir: Name of torrent directory.
+        announce_id: User announce id to use when creating the torrent.
+    """
 
-    announce_id = 'ef53de84b2beea212a4ee452cd9edb19'
     announce_url = f'https://flacsfor.me/{announce_id}/announce'
-    torrent_dir = '/home/jacob/torrents'
 
-    torrent_cmd = f'''mktorrent -l 17 -p -s RED -a {announce_url} "{mp3_path}"
-    -o "{os.path.join(torrent_dir, output_title)}.torrent"'''
+    torrent_cmd = (f'mktorrent -l 17 -p -s RED -a {announce_url} "{input_dir}"'
+                   f' -o "{torrent_dir}.torrent"')
 
     subprocess.run(shlex.split(torrent_cmd), check=True)
 
